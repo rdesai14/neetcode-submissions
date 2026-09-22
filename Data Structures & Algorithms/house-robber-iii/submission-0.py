4f6867047e96b3e946f35f4300fef7e05487229f
Rishi Desai <71404284+rdesai14@users.noreply.github.com>
2026-09-22 13:39:31 -0400

    Add: house-robber-iii - submission-0

---
+++ Data Structures & Algorithms/house-robber-iii/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def dfs(root):
+            if not root:
+                return [0, 0]
+
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+
+            withRoot = root.val + leftPair[1] + rightPair[1]
+            withoutRoot = max(leftPair) + max(rightPair)
+
+            return [withRoot, withoutRoot]
+
+        return max(dfs(root))
